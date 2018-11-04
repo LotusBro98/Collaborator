7483fd86a4c83544a4345b34b1bd6e8c0e78e6ae
LotusBro <LotusBro98@yandex.ru>
2018-11-04 03:56:14 +0300

    OOOOh i don;t believe it's working. AA AAAA then i becam camfrtably nam.

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CPPFLAGS=-pthread
+SHELL=bash
 
 all: main
 
 test: main
-	sudo perf stat -e cpu-cycles,task-clock ./main 1
-	sudo perf stat -e cpu-cycles,task-clock ./main 2
+#	sudo perf stat -e cpu-cycles,task-clock ./main 1
+	@for i in 1 2 3 4 5 6 12 120; do \
+		TIME=`/usr/bin/time -o >(cat) -f "%e" ./main $$i > /dev/null`; \
+		echo -e "$$i: \t$$TIME"; \
+	done
 
 clean:
 	rm main
--- main.cpp
+++ main.cpp
@@ -5,16 +5,81 @@
 #include <pthread.h>
 
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <mutex>
 #include <cmath>
 #include <iomanip>
+#include <stdexcept>
+#include <chrono>
 
 #include <errno.h>
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/time.h>
+
+void calcSums(double (*f)(double x), double from, double to, double * s_out, double * S_out)
+{
+	const int nSeg = 0x1000;
+	const int nSubSeg = 0x10;
+
+	double Dx = (to - from) / nSeg;
+	double dx = Dx / nSubSeg;
+
+	double s = 0;
+	double S = 0;
+
+
+	for (int i = 0; i < nSeg; i++)
+	{
+		double x0 = from + i * Dx;
+		double x1 = x0 + Dx;
+		double f0 = f(x0);
+		double f1 = f(x1);
+		double Df = f1 - f0;
+		double k = Df / nSubSeg;
+
+		double gm = f((x0 + x1) / 2) - (f0 + f1) / 2;
+		double a = 4 * gm / nSubSeg / nSubSeg;
+		
+		double fx = 0;
+		double fx_dx;
+
+		for (int j = 1; j <= nSubSeg; j++)
+		{
+			double x = x0 + j * dx;	
+			fx_dx = fx;
+			fx = f(x) - f0 - k * j - a * j * (j - nSubSeg);
+
+			if ((fx > fx_dx) == (dx > 0)) {
+				s += fx_dx * dx;
+				S += fx * dx;
+			}  else {
+				s += fx * dx;
+				S += fx_dx * dx;
+			}
+		}
+
+		double DI = 0;
+		DI += Dx * (f0 + f1) / 2;
+		DI += a / dx / dx * Dx * Dx * Dx / 6;
+
+		s += DI;
+		S += DI;
+	}
+
+	*s_out = s;
+	*S_out = S;
+}
+
+auto startTime = std::chrono::high_resolution_clock::now();
+
+long long getUsec()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() -startTime).count();
+}
+
 
 class Task
 {
@@ -41,15 +106,17 @@ public:
 		this->from = from;
 		this->to = to;
 
+		loadCpuData();
+
 		push(Seg{from, to});
 	}
 
 
 	friend std::ostream& operator << (std::ostream & os, Task * task)
 	{
-		os << std::setprecision(8) << task->I << "; " << std::setprecision(2);
+		os << std::setprecision(8) << task->I << "; " << task->getStored() << "\n" << std::setprecision(8);
 		for (int i = task->head; i != task->tail; i = (i + 1) % task->size)
-			os << "[" << task->queue[i].from << ", " << task->queue[i].to << "] ";
+			os << "\t[" << task->queue[i].from << ", " << task->queue[i].to << "]\n";
 		os << std::endl;
 
 		return os;
@@ -58,11 +125,8 @@ public:
 	void printProgress()
 	{
 		lock.lock();
-		if (firstPrint)
-			firstPrint = false;
-		else
-			printf("\033M");
-		printf("%d%%\n", (int)(done * 100));
+		fprintf(stderr, "%d%%\n", (int)(done * 100));
+		fprintf(stderr, "\033M");
 		lock.unlock();
 	}
 
@@ -70,21 +134,30 @@ public:
 	{
 		lock.lock();
 		taken--;
-		if (S - s < eps * (seg.to - seg.from) / (to - from))
+		if ((S - s) < eps * (seg.to - seg.from) / (to - from))
 		{
 			I += (s + S) / 2;
 			done += (seg.to - seg.from) / (to - from);
 			
-			lock.unlock();
 			waiting.unlock();
+			lock.unlock();
 			return true;
 		}
 		else
 		{
 			if (!splitAndPush(seg))
-				throw;
-			lock.unlock();
+			{
+				findAdjacentAndMerge();
+				if (!splitAndPush(seg))
+				{
+					std::cerr << this;
+					lock.unlock();
+					throw std::overflow_error("Segment queue overflow. Try specifieng higher eps.");
+				}
+			}
+			
 			waiting.unlock();
+			lock.unlock();
 			return false;
 		}
 	}
@@ -117,14 +190,65 @@ public:
 		}
 	}
 
-	double getI()
+	void loadCpuData()
 	{
-		return I;
+		FILE * lscpu;
+		
+		int maxCore = -1;
+		int core;
+		nCpus = 0;
+
+		lscpu = popen("lscpu -p=core | tail +5", "r");
+		while (fscanf(lscpu, "%d", &core) == 1)
+		{
+			nCpus++;
+			if (core > maxCore)
+				maxCore = core;
+		}
+		pclose(lscpu);
+	
+		int * cores = new int[nCpus];
+
+		lscpu = popen("lscpu -p=core | tail +5", "r");
+		for (int i = 0; i < nCpus; i++)
+		{
+			fscanf(lscpu, "%d", &(cores[i]));
+		}
+		pclose(lscpu);
+
+		cpus = new int[nCpus];
+		
+		core = 0;
+		for (int i = 0; i < nCpus;)
+		{
+			for (int j = 0; j < nCpus; j++)
+			{
+				if (cores[j] == core)
+				{
+					cpus[i] = j;
+					cores[j] = -1;
+					i++;
+					break;
+				}
+			}
+			core = (core + 1) % (maxCore + 1);
+		}
+
+		delete [] cores;
+
 	}
 
-	friend void threadFunc(Task * task, int thr, int nThr);
+	int getCpu(int thr)
+	{
+		return cpus[thr % nCpus];
+	}
 
+	int getStored()
+	{
+		return (tail - head + size) % size;
+	}
 
+	friend double integrate(double (*f)(double x), double from, double to, double eps, int nThreads);
 
 private:
 
@@ -150,16 +274,6 @@ private:
 		return true;
 	}
 
-	inline bool isEmpty()
-	{
-		return (head == tail);
-	}
-
-	inline bool isFull()
-	{
-		return ((tail + 1) % size == head);
-	}
-
 	bool splitAndPush(Seg seg)
 	{
 		if ((tail + 1) % size == head || (tail + 2) % size == head)
@@ -181,145 +295,104 @@ private:
 		return true;
 	}
 
+	bool findAdjacentAndMerge()
+	{
+		int attempt = 0;
+		int successful = 0;
+		for (; attempt < getStored(); attempt++)
+		{
+			Seg seg1;
+			pop(&seg1);
+			
+			int i = head;
+			for (; i != tail; i = (i + 1) % size)
+			{
+				if (seg1.from == queue[i].to)
+				{
+					queue[i].to = seg1.to;
+					break;
+				}
+				else if (seg1.to == queue[i].from)
+				{
+					queue[i].from = seg1.from;
+					break;
+				}
+			}
+
+			if (i != tail)
+				successful++;
+			else
+				push(seg1);
+		}
+		
+		return successful > 0;
+	}
+
+	void threadIntegrate(int thr)
+	{
+		double s = 0;
+		double S = 0;
+
+		cpu_set_t set;
+		CPU_ZERO(&set);
+		CPU_SET(getCpu(thr), &set);
+		pthread_setaffinity_np(pthread_self(), sizeof(set), &set);
+
+		Task::Seg seg;
+
+		long wasted = -getUsec();
+		int iter  = 0;
+
+		while (take(&seg))
+		{
+			wasted += getUsec();
+			
+			calcSums(f, seg.from, seg.to, &s, &S);
+			
+			wasted -= getUsec();
+			commit(seg, s, S);
+			printProgress();
+			iter++;
+		}
+		waiting.unlock();
+		wasted += getUsec();
+
+		fprintf(stderr, "thread %d: %d iterations, %ld ms wasted\n", thr, iter, wasted / 1000);
+	}
 
 
 	int head;
 	int tail;
 	int taken;
-	static const int size = 0x10000;
+	static const int size = 0x1000;
 	std::mutex lock;
 	std::mutex waiting;
 
-	bool firstPrint = true;
-
-	double I;
+	long double I;
 	double done;
 
 	double (*f)(double x);
 	double eps;
 	double from;
 	double to;
+
+	int * cpus;
+	int nCpus;
 	
 	Seg queue[size];
 };
 
-void calcSums(double (*f)(double x), double from, double to, double * s_out, double * S_out)
+
+
+double integrate(double (*f)(double x), double from, double to, double eps, int nThreads)
 {
-	const int nSeg = 0x1000;
-	const int nSubSeg = 0x10;
+	Task * task = new Task(f, from, to, eps);
 
-	double Dx = (to - from) / nSeg;
-	double dx = Dx / nSubSeg;
-
-	double s = 0;
-	double S = 0;
-
-
-	for (int i = 0; i < nSeg; i++)
-	{
-		double x0 = from + i * Dx;
-		double x1 = x0 + Dx;
-		double f0 = f(x0);
-		double f1 = f(x1);
-		double Df = f1 - f0;
-		double k = Df / Dx;
-
-		double fx = 0;
-		double fx_dx;
-
-		for (int j = 1; j <= nSubSeg; j++)
-		{
-			double x = x0 + j * dx;	
-			fx_dx = fx;
-			fx = f(x) - f0 - k * (x - x0);
-
-			if (fx > fx_dx) {
-				s += fx_dx * dx;
-				S += fx * dx;
-			}  else {
-				s += fx * dx;
-				S += fx_dx * dx;
-			}
-		}
-
-		s += Dx * (f0 + f1) / 2;
-		S += Dx * (f0 + f1) / 2;
-	}
-
-	*s_out = s;
-	*S_out = S;
-}
-
-int cpus[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-
-
-
-void threadFunc(Task * task, int thr, int nThr)
-{
-	double s = 0;
-	double S = 0;
-
-	int cpu = cpus[thr % 12];
-
-	cpu_set_t set;
-	CPU_ZERO(&set);
-	CPU_SET(cpu, &set);
-	
-	pthread_setaffinity_np(pthread_self(), sizeof(set), &set);
-
-	Task::Seg seg;
-
-	int i = 0;
-
-	struct timeval tv;
-	
-	bool canTake;
-
-	long wasted = 0;
-	long wastedHere;
-
-	while (true)
-	{
-		wastedHere = 0;
-		gettimeofday(&tv, NULL);
-		wastedHere -= (tv.tv_usec + 1000000 * tv.tv_sec);
-		
-		canTake = task->take(&seg);
-		
-		gettimeofday(&tv, NULL);
-		wastedHere += (tv.tv_usec + 1000000 * tv.tv_sec);
-		if (!canTake)
-			break;
-
-		calcSums(task->f, seg.from, seg.to, &s, &S);
-		
-		gettimeofday(&tv, NULL);
-		wastedHere -= (tv.tv_usec + 1000000 * tv.tv_sec);
-		
-		task->commit(seg, s, S);
-		
-		gettimeofday(&tv, NULL);
-		wastedHere += (tv.tv_usec + 1000000 * tv.tv_sec);
-		
-		i++;
-		if (i % 1 == 0)
-			task->printProgress();
-
-		wasted += wastedHere;
-		//usleep(wastedHere);
-	}
-	task->waiting.unlock();
-
-	printf("thread %d: %d iterations; wasted %d msec\n", thr, i, (int)(wasted / 1000));
-}
-
-double integrate(Task * task, int nThreads)
-{
 	std::thread ** threads = new std::thread*[nThreads];
 
 	for (int i = 0; i < nThreads; i++)
 	{
-		threads[i] = new std::thread(threadFunc, task, i, nThreads);
+		threads[i] = new std::thread(&Task::threadIntegrate, task, i);
 	}
 
 	for (int i = 0; i < nThreads; i++)
@@ -330,8 +403,9 @@ double integrate(Task * task, int nThreads)
 
 	delete [] threads;
 
-	return task->getI();
+	return task->I;
 }
+
 
 
 
@@ -341,32 +415,49 @@ inline double func(double x)
 	return std::sin(x);
 }
 
-
-
-
 int main(int argc, char* argv[])
 {
-	int N = 1;
-	if (argc == 2)
-		N = atoi(argv[1]);
+	const char * usage = "Usage: ./main <n_threads> [<from> <to>] [eps]";
+
+	if (argc != 2 && argc != 4 && argc != 5)
+	{
+		std::cerr << usage << std::endl;
+		return 1;
+	}
+		
+	int N = atoi(argv[1]);
 
 	double eps = 1e-13;
 	double I;
 	
-	double from = 0;
-	double to = 13 * 3.141592653;
+	double from = 0 * M_PI;
+	double to = 7 * M_PI;
 
-	Task * task = new Task(func, from, to, eps);
+	if (argc > 2)
+	{
+		from = atof(argv[2]);
+		to = atof(argv[3]);
+	}
 
-	I = integrate(task, N);
+	if (argc > 4)
+	{
+		eps = std::atof(argv[4]);
+		if (eps < 1e-13)
+			throw std::underflow_error( "Too small eps for type double. Try specifying higher eps.");
+	}
 
-/*
-	double s, S;
-	calcSums(func, from, to, &s, &S);
-	I = (s + S) / 2;
-*/
+	if (errno != 0)
+	{
+		std::cerr << "Failed to parse arguments" << std::endl;
+		std::cerr << usage << std::endl;
+		return 1;
+	}
 
-	std::cout << "I = " << I << std::endl;
+	I = integrate(func, from, to, eps, N);
+
+	std::cout << 
+		std::fixed << std::setprecision(-std::log10(eps)) << "I = " << I << " +/- " << 
+		std::scientific << std::setprecision(0) << eps << std::endl;
 
 	return 0;
 }
